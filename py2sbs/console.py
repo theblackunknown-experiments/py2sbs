d7d5938040d7b2b271c059503f141e713b2b1e12
MACHIZAUD Andréa <andrea.machizaud@gmail.com>
2019-12-11 22:58:04 +0100

    pretty printer from https://bitbucket.org/takluyver/greentreesnakes/src/default/astpp.py

--- py2sbs/console.py
+++ py2sbs/console.py
@@ -15,6 +15,44 @@ from py2sbs import (
 
 SETTINGS_FOLDER = Path.home() / '.py2sbs' / py2sbsversion
 
+# cf. https://bitbucket.org/takluyver/greentreesnakes/src/default/astpp.py
+def dump(node, annotate_fields=True, include_attributes=False, indent='  '):
+    """
+    Return a formatted dump of the tree in *node*.  This is mainly useful for
+    debugging purposes.  The returned string will show the names and the values
+    for fields.  This makes the code impossible to evaluate, so if evaluation is
+    wanted *annotate_fields* must be set to False.  Attributes such as line
+    numbers and column offsets are not dumped by default.  If this is wanted,
+    *include_attributes* can be set to True.
+    """
+    def _format(node, level=0):
+        if isinstance(node, ast.AST):
+            fields = [(a, _format(b, level)) for a, b in ast.iter_fields(node)]
+            if include_attributes and node._attributes:
+                fields.extend([(a, _format(getattr(node, a), level))
+                               for a in node._attributes])
+            return ''.join([
+                node.__class__.__name__,
+                '(',
+                ', '.join(('%s=%s' % field for field in fields)
+                           if annotate_fields else
+                           (b for a, b in fields)),
+                ')'])
+        elif isinstance(node, list):
+            lines = ['[']
+            lines.extend((indent * (level + 2) + _format(x, level + 2) + ','
+                         for x in node))
+            if len(lines) > 1:
+                lines.append(indent * (level + 1) + ']')
+            else:
+                lines[-1] += ']'
+            return '\n'.join(lines)
+        return repr(node)
+
+    if not isinstance(node, ast.AST):
+        raise TypeError('expected AST, got %r' % node.__class__.__name__)
+    return _format(node)
+
 def main():
     parser = argparse.ArgumentParser(description=py2sbsdoc)
     parser.add_argument(
@@ -40,12 +78,12 @@ def main():
         logger.setLevel( logging.DEBUG )
 
     try:
-        source_ast = ast.parse(
+        ast_root_node = ast.parse(
             source=arguments.source_file.read(),
             filename=getattr(arguments.source_file, 'name', '<string>'),
         )
 
-        print(source_ast)
+        logger.info(dump(ast_root_node))
 
         return 0
     except (ValueError, SyntaxError) as e:
@@ -61,4 +99,5 @@ if __name__ == '__main__':
         style='{'
     )
 
+    import sys
     sys.exit(main())
