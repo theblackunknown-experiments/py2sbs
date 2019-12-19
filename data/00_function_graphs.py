f64bb5b0e90d5628fd0c3eff530bf4a161af73ed
MACHIZAUD Andréa <andrea.machizaud@gmail.com>
2019-12-19 09:39:16 +0100

    simpler data 00

--- data/00_function_graphs.py
+++ data/00_function_graphs.py
@@ -1,7 +1,5 @@
 """Sample 00 - showcase how function definition turn into FunctionGraph"""
 
-import sbs
-
 def add(x, y):
     return x + y
 
@@ -11,10 +9,8 @@ def mod(x, y : int):
 def pow(exponent, base = 2, x = 5):
     return base ** exponent
 
-@sbs.pixelprocessor
 def color_pixelprocessor():
     return ( 1.0, 0.0, 1.0, 1.0 )
 
-@sbs.pixelprocessor
 def grayscale_pixelprocessor():
     return 0.5
